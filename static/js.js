f13e613a63ee5781ae15595cee40fdb325f2560e
ivankahf <100836323+ivankahf@users.noreply.github.com>
2023-05-18 10:37:48 +0200

    1

--- static/js.js
+++ static/js.js
@@ -2,10 +2,3 @@ lightbox.option({
   'resizeDuration': 200,
   'wrapAround': true
 });
-
-const lightbox = document.querySelector('#lightbox');
-const backBtn = document.querySelector('#back-btn');
-
-backBtn.addEventListener('click', () => {
-  lightbox.style.display = 'none';
-});
